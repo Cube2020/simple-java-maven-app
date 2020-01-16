d2ab604fe3ba2908a8776c4c7d082926ad207041
nanchangchaofen <jianliang8520@163.com>
2020-01-16 18:51:27 +0800

    Jenkins-0116002: Automatic trigger

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println("Test Github automatic trigger");
+        System.out.println("Hi, Test Github automatic trigger");
     }
 
     private final String getMessage() {
